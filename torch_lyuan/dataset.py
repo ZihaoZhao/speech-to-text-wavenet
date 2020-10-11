7b203a4d8b73a21ccfc4ce2ed67a7f8f40f88ce1
lovvtin <49585883+lovvtin@users.noreply.github.com>
2020-10-11 21:01:15 +0800

    Update dataset.py
    fix some bug

--- torch_lyuan/dataset.py
+++ torch_lyuan/dataset.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 
 
+
 class VCTK(Dataset):
     def __init__(self, cfg, mode):
         self.cfg = cfg
@@ -15,7 +16,12 @@ class VCTK(Dataset):
         if not os.path.exists(self.cfg.datalist):
             raise ValueError('datalist must exists, initial datalist is not supported')
         self.train_filenames, self.test_filenames = json.load(open(self.cfg.datalist, 'r', encoding='utf-8'))
-
+        if self.mode == 'train':
+            self.max_wave = 520
+            self.max_text = 256
+        else:
+            self.max_wave = 720
+            self.max_text = 256
 
     def __getitem__(self, idx):
         if self.mode =='train':
@@ -26,14 +32,14 @@ class VCTK(Dataset):
         txt_path = self.cfg.dataset + filenames[1]
         wave_tmp = utils.read_wave(wave_path) # numpy
         wave_tmp = torch.from_numpy(wave_tmp)
-        wave = torch.zeros([20,512]) # 512 may be too short, if error,fix it
+        wave = torch.zeros([20,self.max_wave]) # 512 may be too short, if error,fix it
         length_wave = wave_tmp.shape[1]
         wave[:,:length_wave] = wave_tmp
 
         text_tmp = utils.read_txt(txt_path)  # list
         length_text = len(text_tmp)
         text_tmp = torch.tensor(text_tmp)
-        text = torch.zeros([256]) # 256 may be too short, fix it, if error
+        text = torch.zeros([self.max_text]) # 256 may be too short, fix it, if error
         text[:length_text] = text_tmp
         name = filenames[0].split('/')[-1]
 
@@ -52,20 +58,24 @@ if __name__ == '__main__':
     # train_filenames, test_filenames = json.load(open('/lyuan/code/speech-to-text-wavenet/data/list.json', 'r', encoding='utf-8'))
     # print(len(train_filenames), train_filenames) #[['/VCTK-Corpus/wav48/p376/p376_076.wav', '/VCTK-Corpus/txt/p376/p376_076.txt'], ['/VCTK-Corpus/wav48/p376/p376_021.wav', '/VCTK-Corpus/txt/p376/p376_021.txt']]
     import config_train as cfg
-    vctk = VCTK(cfg, 'train')
-    length = len(vctk)
-    max_length = 0
-    for i in range(length):
-        tmp = vctk[i]['wave'].shape[1]
-        if tmp>max_length:
-            max_length = tmp
-    print(f'train set {max_length}')
+    # vctk = VCTK(cfg, 'train')
+    # length = len(vctk)
+    # max_length = 0
+    # for i in range(length):
+    #     tmp = vctk[i]['wave'].shape[1]
+    #     if tmp>max_length:
+    #         max_length = tmp
+    # print(f'train set {max_length}')
     vctk = VCTK(cfg, 'val')
     length = len(vctk)
-    max_length = 0
+    max_wave = 0
+    max_text = 0
     for i in range(length):
-        tmp = vctk[i]['wave'].shape[1]
-        if tmp>max_length:
+        tmp = vctk[i]['length_wave']
+        if tmp>max_wave:
             max_length = tmp
-    print(f'val set {max_length}')
+        tmp = vctk[i]['length_text']
+        if tmp > max_text:
+            max_length = tmp
+    print(f'val set {max_wave}, {max_text}')
 
