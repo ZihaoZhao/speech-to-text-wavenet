adf9476da87720ae2377d3d3b3b7431bc2d26826
竹杰 <zhujie.zzh@alibaba-inc.com>
2020-10-23 07:44:19 +0800

    update train.sh

--- torch_lyuan/train.sh
+++ torch_lyuan/train.sh
@@ -1,7 +1,18 @@
+###
+ # @Author       : Zihao Zhao
+ # @E-mail       : zhzhao18@fudan.edu.cn
+ # @Company      : Fudan University
+ # @Date         : 2020-10-13 20:16:46
+ # @LastEditors  : Zihao Zhao
+ # @LastEditTime : 2020-10-23 07:43:59
+ # @FilePath     : /speech-to-text-wavenet/torch_lyuan/train.sh
+ # @Description  : 
+### 
 #!/usr/bin/env bash
 
 
-/zhzhao/miniconda3/bin/conda init | conda activate pytorch16 
+#/zhzhao/miniconda3/bin/conda init bash | conda activate pytorch16 | sh /zhzhao/code/wavenet_torch/torch_lyuan/train.sh
+
 /zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp pt_16_1616_128 --sparse_mode pattern_pruning --pattern_para 16_16_16_128 --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth 
 /zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp pt_16_1616_192 --sparse_mode pattern_pruning --pattern_para 16_16_16_192 --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth 
 /zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp pt_16_1616_64 --sparse_mode pattern_pruning --pattern_para 16_16_16_64 --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth  
