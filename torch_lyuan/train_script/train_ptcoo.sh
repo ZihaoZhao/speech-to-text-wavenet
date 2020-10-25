7636949f5560ebde490aa1bd01a6da15e3fa6e7b
竹杰 <zhujie.zzh@alibaba-inc.com>
2020-10-25 10:33:14 +0800

    fix: train scripts bug

--- torch_lyuan/train_script/train_ptcoo.sh
+++ torch_lyuan/train_script/train_ptcoo.sh
@@ -3,9 +3,9 @@
  # @E-mail       : zhzhao18@fudan.edu.cn
  # @Company      : Fudan University
  # @Date         : 2020-10-13 20:16:46
- # @LastEditors: Please set LastEditors
- # @LastEditTime: 2020-10-24 08:59:47
- # @FilePath     : /speech-to-text-wavenet/torch_lyuan/train.sh
+ # @LastEditors  : Zihao Zhao
+ # @LastEditTime : 2020-10-25 10:32:11
+ # @FilePath     : /speech-to-text-wavenet/torch_lyuan/train_script/train_ptcoo.sh
  # @Description  : 
 ### 
 #!/usr/bin/env bash
@@ -19,4 +19,4 @@
 /zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp ptcoo_16_16_16_96_32 --sparse_mode ptcoo_pruning  --ptcoo_para 16_16_16_96_32 --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth --lr 0.001   --skip_exist --save_excel ptcoo.xls
 /zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp ptcoo_16_16_16_72_24 --sparse_mode ptcoo_pruning  --ptcoo_para 16_16_16_72_24 --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth --lr 0.001   --skip_exist --save_excel ptcoo.xls
 /zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp ptcoo_16_16_16_48_16 --sparse_mode ptcoo_pruning  --ptcoo_para 16_16_16_48_16 --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth --lr 0.001   --skip_exist --save_excel ptcoo.xls
-/zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp ptcoo_16_16_16_24_8 --sparse_mode ptcoo_pruning   --ptcoo_para 16_16_16_24_8 ---load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth --lr 0.001   --skip_exist --save_excel ptcoo.xls
+/zhzhao/miniconda3/envs/pytorch16/bin/python /zhzhao/code/wavenet_torch/torch_lyuan/train.py --exp ptcoo_16_16_16_24_8 --sparse_mode ptcoo_pruning   --ptcoo_para 16_16_16_24_8  --load_from /zhzhao/code/wavenet_torch/torch_lyuan/exp_result/best.pth --lr 0.001   --skip_exist --save_excel ptcoo.xls
